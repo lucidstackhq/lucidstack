92e8901e17425306f40748919c6934e1dd5c49bb
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-06-20 12:18:11 +0530

    added login ui

--- src/main/java/xyz/lucidstack/controller/PageController.java
+++ src/main/java/xyz/lucidstack/controller/PageController.java
@@ -10,4 +10,14 @@ public class PageController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "logout";
+    }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -5,3 +5,36 @@ function displayError(parent, message) {
 function displaySuccess(parent, message) {
     parent.html(`<div class="alert alert-success">${message}</div>`);
 }
+
+function userSignUp(username, password, organizationName, billingEmail, success, error) {
+    $.ajax({
+        url: "/api/v1/users/signup",
+        method: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({
+            username,
+            password,
+            organizationName,
+            billingEmail,
+        }),
+        success: success,
+        error: error,
+    })
+}
+
+function getUserToken(username, password, organizationName, success, error) {
+    $.ajax({
+        url: "/api/v1/users/token",
+        method: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({
+            username,
+            password,
+            organizationName,
+        }),
+        success: success,
+        error: error,
+    })
+}
