ee208193cb1954b41347f5a164d660fcdebc0ca0
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-06-20 11:31:56 +0530

    save

---
+++ src/main/java/xyz/lucidstack/model/EntityEvent.java
@@ -0,0 +1,4 @@
+package xyz.lucidstack.model;
+
+public class EntityEvent {
+}
---
+++ src/main/java/xyz/lucidstack/model/Model.java
@@ -0,0 +1,21 @@
+package xyz.lucidstack.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(NON_NULL)
+@Document
+public class Model {
+}
